e91a389c85af187255850a9f0e802266158d21f8
PedroQueiroz1 <pedroqueiroz0411@outlook.com.br>
2022-12-22 21:29:03 -0300

    Finishes BookService.class

--- 05_FirstStepsInJavaWithSpringBoot/rest-with-spring-boot-and-java/src/main/java/br/com/project/services/BookService.java
+++ 05_FirstStepsInJavaWithSpringBoot/rest-with-spring-boot-and-java/src/main/java/br/com/project/services/BookService.java
@@ -9,6 +9,7 @@ import org.springframework.stereotype.Service;
 
 import br.com.project.controllers.BookController;
 import br.com.project.data.vo.v1.BookVO;
+import br.com.project.exceptions.RequiredObjectIsNullException;
 import br.com.project.exceptions.ResourceNotFoundException;
 import br.com.project.mapper.DozerMapper;
 import br.com.project.mapper.custom.BookMapper;
@@ -48,5 +49,47 @@ public class BookService {
 		BookVO vo = DozerMapper.parseObject(entity, BookVO.class);
 		vo.add(linkTo(methodOn(BookController.class).findById(id)).withSelfRel());
 		return vo;
+	}
+	
+	// --------- CREATE ------------
+	public BookVO create(BookVO book) {
+		
+		if(book == null) throw new RequiredObjectIsNullException();
+		logger.info("Creating a Book!");
+		
+		var entity = DozerMapper.parseObject(book, Book.class);
+		BookVO vo = DozerMapper.parseObject(bookRepository.save(entity), BookVO.class);
+		vo.add(linkTo(methodOn(BookController.class).findById(vo.getKey())).withSelfRel());
+		
+		return vo;
+	}
+	
+	// --------- UPDATE ------------
+	public BookVO update(BookVO book) {
+		logger.info("Updating one Book!");
+		
+		var entity = bookRepository.findById(book.getKey())
+				.orElseThrow(() -> new ResourceNotFoundException("No records found for this ID!"));
+		
+		entity.setTitle(book.getTitle());
+		entity.setTheme(book.getTheme());
+		entity.setAuthor(book.getAuthor());
+		entity.setPageQuantity(book.getPageQuantity());
+		entity.setDifficultyToRead(book.getDifficultyToRead());
+		
+		BookVO vo = DozerMapper.parseObject(bookRepository.save(entity), BookVO.class);
+		vo.add(linkTo(methodOn(BookController.class).findById(vo.getKey())).withSelfRel());
+				
+		return vo;
+	}
+	
+	// --------- DELETE ------------
+	public void delete(Long id) {
+		logger.info("Deleting one Book!");
+		
+		var entity = bookRepository.findById(id)
+				.orElseThrow(() -> new ResourceNotFoundException("No records found for this ID!"));
+		
+		bookRepository.delete(entity);
 	}
 }
