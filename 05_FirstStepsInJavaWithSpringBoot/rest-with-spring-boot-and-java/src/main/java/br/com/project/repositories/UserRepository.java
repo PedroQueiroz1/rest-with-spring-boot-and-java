77d13a27fb37de6fbad8560bf0dfd379c09b65ef
PedroQueiroz1 <pedroqueiroz0411@outlook.com.br>
2023-01-15 15:20:39 -0300

    Adds UserRepository

---
+++ 05_FirstStepsInJavaWithSpringBoot/rest-with-spring-boot-and-java/src/main/java/br/com/project/repositories/UserRepository.java
@@ -0,0 +1,17 @@
+package br.com.project.repositories;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
+import org.springframework.stereotype.Repository;
+
+import br.com.project.model.Person;
+import br.com.project.model.User;
+
+
+@Repository
+public interface UserRepository extends JpaRepository<User, Long> {
+
+	@Query("SELECT u FROM User WHERE u.userName =: userName")
+	User findByUsername(@Param("userName") String userName);
+}
