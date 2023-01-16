7029ed6de2244e29523427cd50ae4b1aefcea8a6
PedroQueiroz1 <pedroqueiroz0411@outlook.com.br>
2023-01-16 17:55:57 -0300

    Adds AuthController

---
+++ 05_FirstStepsInJavaWithSpringBoot/rest-with-spring-boot-and-java/src/main/java/br/com/project/controllers/AuthController.java
@@ -0,0 +1,41 @@
+package br.com.project.controllers;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import br.com.project.data.vo.v1.security.AccountCredentialsVO;
+import br.com.project.services.AuthService;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.parameters.RequestBody;
+import io.swagger.v3.oas.annotations.tags.Tag;
+
+@Tag(name = "Authentication Endpoint")
+@RestController
+@RequestMapping("/auth")
+public class AuthController {
+
+	@Autowired
+	AuthService authService;
+	
+	@SuppressWarnings("rawtypes")
+	@Operation(summary = "Authenticates an user and return a token")
+	@PostMapping(value = "/signin")
+	public ResponseEntity signin(@RequestBody AccountCredentialsVO data) {
+		if (checkIfParamsIsNotNull(data))
+			return ResponseEntity.status(HttpStatus.FORBIDDEN).body("Invalid client request!");
+		var token = authService.signin(data);
+		if (token == null)
+			return ResponseEntity.status(HttpStatus.FORBIDDEN).body("Invalid client request!");
+		return token;
+	}
+
+	private boolean checkIfParamsIsNotNull(AccountCredentialsVO data) {
+		return data == null || data.getUsername() == null || 
+			data.getUsername().isBlank() || data.getPassword().isBlank();
+	}
+	
+}
