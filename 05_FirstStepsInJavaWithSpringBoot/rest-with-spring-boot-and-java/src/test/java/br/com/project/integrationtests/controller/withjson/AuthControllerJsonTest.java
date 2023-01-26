2bce3d01f8564d9197ac8ed5e75dbe01e9086f51
PedroQueiroz1 <pedroqueiroz0411@outlook.com.br>
2023-01-26 00:38:35 -0300

    Adds testRefresh method to AuthControllerJsonTest

--- 05_FirstStepsInJavaWithSpringBoot/rest-with-spring-boot-and-java/src/test/java/br/com/project/integrationtests/controller/withjson/AuthControllerJsonTest.java
+++ 05_FirstStepsInJavaWithSpringBoot/rest-with-spring-boot-and-java/src/test/java/br/com/project/integrationtests/controller/withjson/AuthControllerJsonTest.java
@@ -15,16 +15,13 @@ import com.fasterxml.jackson.core.JsonProcessingException;
 import com.fasterxml.jackson.databind.JsonMappingException;
 
 import br.com.project.configs.TestConfigs;
+import br.com.project.integrationtests.testcontainers.AbstractIntegrationTest;
 import br.com.project.integrationtests.vo.AccountCredentialsVO;
 import br.com.project.integrationtests.vo.TokenVO;
-import io.restassured.builder.RequestSpecBuilder;
-import io.restassured.filter.log.LogDetail;
-import io.restassured.filter.log.RequestLoggingFilter;
-import io.restassured.filter.log.ResponseLoggingFilter;
 
 @SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.DEFINED_PORT)
 @TestMethodOrder(OrderAnnotation.class)
-public class AuthControllerJsonTest {
+public class AuthControllerJsonTest extends AbstractIntegrationTest {
 
 	private static TokenVO tokenVO;
 	
@@ -49,6 +46,31 @@ public class AuthControllerJsonTest {
 
 		assertNotNull(tokenVO.getAccessToken());
 		assertNotNull(tokenVO.getRefreshToken());
+		
+	}
+	
+	@Test
+	@Order(2)
+	public void testRefresh() throws JsonMappingException, JsonProcessingException {
+		
+		AccountCredentialsVO user = new AccountCredentialsVO("leandro", "admin123");
+		
+		var newTokenVO = given()
+				.basePath("/auth/refresh")
+				.port(TestConfigs.SERVER_PORT)
+				.contentType(TestConfigs.CONTENT_TYPE_JSON)
+					.pathParam("username", tokenVO.getUsername())
+					.header(TestConfigs.HEADER_PARAM_AUTHORIZATION, "Bearer " + tokenVO.getRefreshToken())
+				.when()
+					.put("{username}")
+				.then()
+					.statusCode(200)
+				.extract()
+					.body()
+						.as(TokenVO.class);
+		
+		assertNotNull(newTokenVO.getAccessToken());
+		assertNotNull(newTokenVO.getRefreshToken());
 		
 	}
 }
