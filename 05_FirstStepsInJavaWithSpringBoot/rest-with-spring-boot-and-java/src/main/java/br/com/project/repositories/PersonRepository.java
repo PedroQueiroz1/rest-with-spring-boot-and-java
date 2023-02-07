cd0e33780d333fbaa6de9d117f7f2099b22d4364
PedroQueiroz1 <pedroqueiroz0411@outlook.com.br>
2023-02-07 20:43:26 -0300

    Adds new query to PersonRepository

--- 05_FirstStepsInJavaWithSpringBoot/rest-with-spring-boot-and-java/src/main/java/br/com/project/repositories/PersonRepository.java
+++ 05_FirstStepsInJavaWithSpringBoot/rest-with-spring-boot-and-java/src/main/java/br/com/project/repositories/PersonRepository.java
@@ -1,6 +1,9 @@
 package br.com.project.repositories;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Modifying;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
 import br.com.project.model.Person;
@@ -9,4 +12,8 @@ import br.com.project.model.Person;
 @Repository
 public interface PersonRepository extends JpaRepository<Person, Long> {
 
+	@Modifying
+	@Query("UPDATE Person p p.enabled = false WHERE p.id =:id")
+	Person disablePerson(@Param("id") Long id);
+	
 }
