17a5402dcb0fb22f8c2be7898783f4355d7a47c0
PedroQueiroz1 <pedroqueiroz0411@outlook.com.br>
2023-02-28 21:15:45 -0300

    Implements a PagedModel return to FindAll's method

--- 05_FirstStepsInJavaWithSpringBoot/rest-with-spring-boot-and-java/src/main/java/br/com/project/controllers/PersonController.java
+++ 05_FirstStepsInJavaWithSpringBoot/rest-with-spring-boot-and-java/src/main/java/br/com/project/controllers/PersonController.java
@@ -8,6 +8,8 @@ import org.springframework.data.domain.PageRequest;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.domain.Sort;
 import org.springframework.data.domain.Sort.Direction;
+import org.springframework.hateoas.EntityModel;
+import org.springframework.hateoas.PagedModel;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.DeleteMapping;
@@ -60,7 +62,7 @@ public class PersonController {
 					@ApiResponse(description = "Internal Error", responseCode = "500", content = @Content)
 		}
 	)
-	public ResponseEntity<Page<PersonVO>> findAll(
+	public ResponseEntity<PagedModel<EntityModel<PersonVO>>> findAll(
 			@RequestParam(value = "page", defaultValue = "0") Integer page,
 			@RequestParam(value = "size", defaultValue = "12") Integer size,
 			@RequestParam(value = "direction", defaultValue = "asc") String direction) 
@@ -68,7 +70,7 @@ public class PersonController {
 		
 		var sortDirection = "desc".equalsIgnoreCase(direction) ? Direction.DESC : Direction.ASC;
 		
-		Pageable pageable = PageRequest.of(page, limit, Sort.by(sortDirection, "firstName"));
+		Pageable pageable = PageRequest.of(page, size, Sort.by(sortDirection, "firstName"));
 		return ResponseEntity.ok(personService.findAll(pageable));
 	}
 	
--- 05_FirstStepsInJavaWithSpringBoot/rest-with-spring-boot-and-java/src/main/java/br/com/project/services/PersonService.java
+++ 05_FirstStepsInJavaWithSpringBoot/rest-with-spring-boot-and-java/src/main/java/br/com/project/services/PersonService.java
@@ -1,13 +1,16 @@
 package br.com.project.services;
 
+import static org.springframework.hateoas.server.mvc.WebMvcLinkBuilder.linkTo;
+import static org.springframework.hateoas.server.mvc.WebMvcLinkBuilder.methodOn;
+
 import java.util.logging.Logger;
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
-
-import static org.springframework.hateoas.server.mvc.WebMvcLinkBuilder.*;
-
+import org.springframework.data.web.PagedResourcesAssembler;
+import org.springframework.hateoas.EntityModel;
+import org.springframework.hateoas.Link;
+import org.springframework.hateoas.PagedModel;
 import org.springframework.stereotype.Service;
 
 import br.com.project.controllers.PersonController;
@@ -29,21 +32,31 @@ public class PersonService {
 	PersonRepository personRepository;
 	
 	@Autowired
+	PagedResourcesAssembler<PersonVO> assembler;
+	
+	@Autowired
 	PersonMapper mapper;
 
 	// --------- FIND-ALL ------------
-	public Page<PersonVO> findAll(Pageable pageable) {
+	public PagedModel<EntityModel<PersonVO>> findAll(Pageable pageable) {
 
 		logger.info("Finding all people!");
 
 		var personPage = personRepository.findAll(pageable);
 		
-		var personVOsPage = personPage.map(p -> DozerMapper.parseObject(p, PersonVO.class));
-		personVOsPage.map(
+		var personVosPage = personPage.map(p -> DozerMapper.parseObject(p, PersonVO.class));
+		personVosPage.map(
 				p -> p.add(
 						linkTo(methodOn(PersonController.class)
 								.findById(p.getKey())).withSelfRel()));
-		return personVOsPage;
+		
+		Link link = linkTo(
+				methodOn(PersonController.class)
+					.findAll(pageable.getPageNumber(),
+							pageable.getPageSize(),
+							"asc")).withSelfRel();
+			
+			return assembler.toModel(personVosPage, link);
 	}
 
 	// --------- FIND-BY-ID ------------
