018687ccd9f4286f3e66563b994f395030694ffa
PedroQueiroz1 <pedroqueiroz0411@outlook.com.br>
2023-03-09 22:58:06 -0300

    Finishes new Wrappers classes

--- 05_FirstStepsInJavaWithSpringBoot/rest-with-spring-boot-and-java/src/test/java/br/com/project/integrationtests/vo/wrappers/PersonEmbeddedVO.java
+++ 05_FirstStepsInJavaWithSpringBoot/rest-with-spring-boot-and-java/src/test/java/br/com/project/integrationtests/vo/wrappers/PersonEmbeddedVO.java
@@ -1,10 +1,63 @@
 package br.com.project.integrationtests.vo.wrappers;
 
 import java.io.Serializable;
+import java.util.List;
+
+import com.fasterxml.jackson.annotation.JsonProperty;
+
+import br.com.project.integrationtests.vo.PersonVO;
 
 public class PersonEmbeddedVO implements Serializable{
 	
 	private static final long serialVersionUID = 1L;
 
-	private PersonEmbeddedVO embedded;
+	@JsonProperty("personVOList")
+	private List<PersonVO> embedded;
+
+	
+	public PersonEmbeddedVO() {
+	}
+
+
+	public PersonEmbeddedVO(List<PersonVO> embedded) {
+		this.embedded = embedded;
+	}
+
+	
+	public List<PersonVO> getEmbedded() {
+		return embedded;
+	}
+
+	public void setEmbedded(List<PersonVO> embedded) {
+		this.embedded = embedded;
+	}
+
+
+	@Override
+	public int hashCode() {
+		final int prime = 31;
+		int result = 1;
+		result = prime * result + ((embedded == null) ? 0 : embedded.hashCode());
+		return result;
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		PersonEmbeddedVO other = (PersonEmbeddedVO) obj;
+		if (embedded == null) {
+			if (other.embedded != null)
+				return false;
+		} else if (!embedded.equals(other.embedded))
+			return false;
+		return true;
+	}
+	
+	
+	
 }
--- 05_FirstStepsInJavaWithSpringBoot/rest-with-spring-boot-and-java/src/test/java/br/com/project/integrationtests/vo/wrappers/WrapperPersonVO.java
+++ 05_FirstStepsInJavaWithSpringBoot/rest-with-spring-boot-and-java/src/test/java/br/com/project/integrationtests/vo/wrappers/WrapperPersonVO.java
@@ -2,6 +2,8 @@ package br.com.project.integrationtests.vo.wrappers;
 
 import java.io.Serializable;
 
+import com.fasterxml.jackson.annotation.JsonProperty;
+
 import jakarta.xml.bind.annotation.XmlRootElement;
 
 @XmlRootElement
@@ -9,5 +11,50 @@ public class WrapperPersonVO implements Serializable{
 	
 	private static final long serialVersionUID = 1L;
 
+	@JsonProperty("_embedded")
 	private PersonEmbeddedVO embedded;
+
+	
+	public WrapperPersonVO() {}
+
+	public WrapperPersonVO(PersonEmbeddedVO embedded) {
+		this.embedded = embedded;
+	}
+	
+
+	public PersonEmbeddedVO getEmbedded() {
+		return embedded;
+	}
+
+	public void setEmbedded(PersonEmbeddedVO embedded) {
+		this.embedded = embedded;
+	}
+
+	
+	@Override
+	public int hashCode() {
+		final int prime = 31;
+		int result = 1;
+		result = prime * result + ((embedded == null) ? 0 : embedded.hashCode());
+		return result;
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		WrapperPersonVO other = (WrapperPersonVO) obj;
+		if (embedded == null) {
+			if (other.embedded != null)
+				return false;
+		} else if (!embedded.equals(other.embedded))
+			return false;
+		return true;
+	}
+	
+	
 }
