fe843dec1990482d55021615fef3502e3fb9e7f7
PedroQueiroz1 <pedroqueiroz0411@outlook.com.br>
2023-03-15 19:18:38 -0300

    Adds BookEmbeddedVO

---
+++ 05_FirstStepsInJavaWithSpringBoot/rest-with-spring-boot-and-java/src/test/java/br/com/project/integrationtests/vo/wrappers/BookEmbeddedVO.java
@@ -0,0 +1,57 @@
+package br.com.project.integrationtests.vo.wrappers;
+
+import java.io.Serializable;
+import java.util.List;
+
+import com.fasterxml.jackson.annotation.JsonProperty;
+
+import br.com.project.integrationtests.vo.BookVO;
+
+public class BookEmbeddedVO implements Serializable{
+
+	private static final long serialVersionUID = 1L;
+	
+	@JsonProperty("bookVOList")
+	private List<BookVO> books;
+	
+
+	public BookEmbeddedVO(List<BookVO> books) {}
+
+	
+	public List<BookVO> getBooks() {
+		return books;
+	}
+
+	public void setBooks(List<BookVO> books) {
+		this.books = books;
+	}
+
+	
+	@Override
+	public int hashCode() {
+		final int prime = 31;
+		int result = 1;
+		result = prime * result + ((books == null) ? 0 : books.hashCode());
+		return result;
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		BookEmbeddedVO other = (BookEmbeddedVO) obj;
+		if (books == null) {
+			if (other.books != null)
+				return false;
+		} else if (!books.equals(other.books))
+			return false;
+		return true;
+	}
+	
+	
+	
+}
