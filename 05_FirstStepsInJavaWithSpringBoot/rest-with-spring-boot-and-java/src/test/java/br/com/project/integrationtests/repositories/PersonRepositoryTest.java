05de28f96a320de42f95dd884e35db1798e662b4
PedroQueiroz1 <pedroqueiroz0411@outlook.com.br>
2023-03-23 18:50:10 -0300

    Adds a findByNameTest to PersonRepositoryTest

--- 05_FirstStepsInJavaWithSpringBoot/rest-with-spring-boot-and-java/src/test/java/br/com/project/integrationtests/repositories/PersonRepositoryTest.java
+++ 05_FirstStepsInJavaWithSpringBoot/rest-with-spring-boot-and-java/src/test/java/br/com/project/integrationtests/repositories/PersonRepositoryTest.java
@@ -1,6 +1,13 @@
 package br.com.project.integrationtests.repositories;
 
+import static io.restassured.RestAssured.given;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertNotNull;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
 import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.Order;
+import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.TestMethodOrder;
 import org.junit.jupiter.api.MethodOrderer.OrderAnnotation;
 import org.junit.jupiter.api.extension.ExtendWith;
@@ -8,9 +15,19 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.jdbc.AutoConfigureTestDatabase;
 import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.domain.Sort;
+import org.springframework.data.domain.Sort.Direction;
 import org.springframework.test.context.junit.jupiter.SpringExtension;
 
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.JsonMappingException;
+
+import br.com.project.configs.TestConfigs;
 import br.com.project.integrationtests.testcontainers.AbstractIntegrationTest;
+import br.com.project.integrationtests.vo.PersonVO;
+import br.com.project.integrationtests.vo.pagedmodels.PagedModelPerson;
 import br.com.project.model.Person;
 import br.com.project.repositories.PersonRepository;
 
@@ -28,5 +45,29 @@ public class PersonRepositoryTest extends AbstractIntegrationTest{
 	@BeforeAll
 	public static void setup() {
 		person = new Person();
+	}
+	
+	@Test
+	@Order(0)
+	public void testFindByName() throws JsonMappingException, JsonProcessingException {
+		
+		Pageable pageable = PageRequest.of(0, 6, Sort.by(Direction.ASC, "firstName"));
+		person = repository.findPersonsByName("ayr", pageable).getContent().get(0);
+		
+		assertNotNull(person.getId());
+		assertNotNull(person.getFirstName());
+		assertNotNull(person.getLastName());
+		assertNotNull(person.getAddress());
+		assertNotNull(person.getGender());
+		
+		assertTrue(person.getEnabled());
+
+		assertEquals(1, person.getId());
+		
+		assertEquals("Ayrton", person.getFirstName());
+		assertEquals("Senna", person.getLastName());
+		assertEquals("São Paulo", person.getAddress());
+		assertEquals("Male", person.getGender());
+	
 	}
 }
