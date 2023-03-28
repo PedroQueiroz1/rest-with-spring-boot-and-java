037f41c27f588ab0409b5b440b20c4a33c08974f
PedroQueiroz1 <pedroqueiroz0411@outlook.com.br>
2023-03-28 20:41:47 -0300

    Adds FileController to controllers package

---
+++ 05_FirstStepsInJavaWithSpringBoot/rest-with-spring-boot-and-java/src/main/java/br/com/project/controllers/FileController.java
@@ -0,0 +1,39 @@
+package br.com.project.controllers;
+
+import java.util.logging.Logger;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.multipart.MultipartFile;
+import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
+
+import br.com.project.data.vo.v1.UploadFileResponseVO;
+import br.com.project.services.FileStorageService;
+import io.swagger.v3.oas.annotations.tags.Tag;
+
+@Tag(name = "File Endpoint")
+@RestController
+@RequestMapping("/api/file/v1")
+public class FileController {
+
+	private Logger logger = Logger.getLogger(FileController.class.getName());
+	
+	@Autowired
+	private FileStorageService service;
+	
+	@PostMapping("/uploadFile")
+	public UploadFileResponseVO uploadFile(@RequestParam("file") MultipartFile file) {
+		logger.info("Storing file to disk");
+		var fileName = service.storeFile(file);
+		String fileDownloadUri = ServletUriComponentsBuilder.fromCurrentContextPath()
+				.path("/api/file/v1/downloadFile/")
+				.path(fileName)
+				.toUriString();
+		return new UploadFileResponseVO(
+				fileName, fileDownloadUri, file.getContentType(), file.getSize());
+	}
+	
+}
