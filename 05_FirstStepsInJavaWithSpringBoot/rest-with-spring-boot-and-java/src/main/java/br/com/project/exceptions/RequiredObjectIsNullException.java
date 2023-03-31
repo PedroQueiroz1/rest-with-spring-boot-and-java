4e00e9c6842cd86925402d5ec9f1d3162b2113b9
PedroQueiroz1 <pedroqueiroz0411@outlook.com.br>
2023-03-31 19:13:34 -0300

    :bug: fix: Fix a bug.

--- 05_FirstStepsInJavaWithSpringBoot/rest-with-spring-boot-and-java/src/main/java/br/com/project/exceptions/RequiredObjectIsNullException.java
+++ 05_FirstStepsInJavaWithSpringBoot/rest-with-spring-boot-and-java/src/main/java/br/com/project/exceptions/RequiredObjectIsNullException.java
@@ -9,7 +9,7 @@ public class RequiredObjectIsNullException extends RuntimeException {
 	private static final long serialVersionUID = 1L;
 	
 	public RequiredObjectIsNullException() {
-		super("It is not allowed to persist a null object!");
+		super("It is not allowed to persist a null object");
 	}
 	
 	public RequiredObjectIsNullException(String ex) {
