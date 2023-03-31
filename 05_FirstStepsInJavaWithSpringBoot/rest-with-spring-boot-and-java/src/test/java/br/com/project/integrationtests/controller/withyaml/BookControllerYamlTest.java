e71abc67cd6ccca09be930f2f54a0f1b6a789cd3
PedroQueiroz1 <pedroqueiroz0411@outlook.com.br>
2023-03-31 18:06:24 -0300

    :poop: chore: Remove an unnecessary java import line.

--- 05_FirstStepsInJavaWithSpringBoot/rest-with-spring-boot-and-java/src/test/java/br/com/project/integrationtests/controller/withyaml/BookControllerYamlTest.java
+++ 05_FirstStepsInJavaWithSpringBoot/rest-with-spring-boot-and-java/src/test/java/br/com/project/integrationtests/controller/withyaml/BookControllerYamlTest.java
@@ -5,7 +5,6 @@ import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertNotNull;
 import static org.junit.jupiter.api.Assertions.assertTrue;
 
-import java.util.Arrays;
 import java.util.Date;
 import java.util.List;
 
